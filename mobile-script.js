54a0eda809047161147ebd3e0dfae0da629f81a9
MONDSKY <r6544083@gmail.com>
2025-06-06 17:09:42 +0800

    Create mobile-script.js

---
+++ mobile-script.js
@@ -0,0 +1,8 @@
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(section => section.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
+
+document.getElementById('theme-toggle').addEventListener('click', () => {
+  document.body.classList.toggle('light');
+});
